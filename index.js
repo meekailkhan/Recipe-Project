3445f4daf0317ddfea0bb95702d84813681419d9
pc <meekailkhan123aar@gmail.com>
2024-10-17 16:08:43 +0530

    add compression

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const path = require("path")
 
 const express = require("express");
 const app = express();
+const compression = require("compression")
 const port = 17000;
+
+app.use(compression())
 
 
 const staticPath = path.join(__dirname,("./route"))
